df8801a3bc410678d6d6a2ede1dd97e4fccca410
vertvern <vertvern21@gmail.com>
2020-11-22 00:24:50 +0300

    user, student declared

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const user = 'John Doe';
-const student = 'Vital';
+const user = {
+  user: 'John Doe',
+  student: 'Vital',
+};
+// при выводе в консоль оба значения- юзер и студент должны появиться в блоке
 console.log(user);
-console.log(student);
+console.log(user.student);
