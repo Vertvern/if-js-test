8b7e384f32223e6bbba337c2cacafc0084a2d123
vertvern <vertvern21@gmail.com>
2020-11-22 05:32:49 +0300

    lesson-2- 8-11 paragraphs.

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,6 @@ module.exports = {
   },
   rules: {
     "no-plusplus": 0 ,
+
   },
 };
--- main.js
+++ main.js
@@ -23,4 +23,27 @@ test = !test;
 // после смены типа значения сперва присвоилось true, затем false
 console.log(test);
 
+// const arr1 = [2, 3, 5, 8];
+// let y = [];
+// for (let x = 0; x < arr1.length; x++) {
+//   y *= arr1[x];
+// }
+// console.log(y);
 
+// не работающее произведение массива
+
+const arr2 = [2, 5, 8, 15, 0, 6, 20, 3];
+for (let x = !5; x < arr2.length; x++) {
+  // как избежать
+  if (arr2[x] >= 5 && arr2[x] <= 10) {
+    console.log(arr2[x]);
+  }
+}
+// вывод чисел от 5 до 10 включительно
+const arr3 = [2, 5, 8, 15, 0, 6, 20, 3];
+for (let x = 0; x < arr3.length; x++) {
+  if ((arr3[x] % 2) === 0) {
+    console.log(arr3[x]);
+  }
+}
+// нахождение и вывод в консоль четных чисел
