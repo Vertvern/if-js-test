663df5edcd20bfa32023357a3bf4aa2e58db9b1d
vertvern <vertvern21@gmail.com>
2021-01-24 15:45:57 +0300

    coloring text by click

---
+++ tests/index.test.js
@@ -0,0 +1,7 @@
+test('toEqual with arrays', (makeCounter) => {
+   expect(makeCounter).toEqual(0); // Ок
+  expect(makeCounter).toEqual(10); // Ошибка
+  expect(colors).toContain(['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue']); // Ок
+});
+/* при запуске появляется ошибка"jest" не является внутренней или внешней
+командой, исполняемой программой или пакетным файлом.*/
